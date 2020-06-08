927b7eb27e0ccd113b5f6aa5f546143957054b13
Tony Galmiche <tony.galmiche@infosaone.com>
2020-06-08 10:11:25 +0200

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Module odoo 13 pour France Filets",
+    "summary": "Module odoo 13 pour France Filets",
+    "version": "13.0.1.0.0",
+    "author": "Tony Galmiche / InfoSaône",
+    "website": "https://infosaone.com",
+    "category": "InfoSaône",
+    "depends": [
+        "mass_mailing",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mailing_contact_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+}
---
+++ models/mailing_contact.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError
+
+
+class IsSegment(models.Model):
+    _name = 'is.segment'
+    _description = "Segment"
+    _order = 'name'
+
+    name = fields.Char("Segment", required=True, index=True)
+
+
+class IsRegion(models.Model):
+    _name = 'is.region'
+    _description = "Région"
+    _order = 'name'
+
+    name = fields.Char("Région", required=True, index=True)
+
+
+class MassMailingContact(models.Model):
+    _inherit = 'mailing.contact'
+
+    is_code_ape      = fields.Char(string='Code APE')
+    is_ville         = fields.Char(string='Ville')
+    is_code_postal   = fields.Char(string='Code Postal')
+    is_rue           = fields.Char(string='Rue')
+    is_region_id     = fields.Many2one('is.region', "Région")
+    is_telephone     = fields.Char(string='Numéro de téléphone')
+    is_fax           = fields.Char(string='Numéro de fax')
+    is_site_internet = fields.Char(string='Site internet')
+    is_segment_id    = fields.Many2one('is.segment', "Segment")
+
+
+    def add_last_mailing_list_action(self):
+        lists = self.env['mailing.list'].search([], limit=1, order='id desc')
+        if len(lists):
+            for obj in self.browse(self.env.context['active_ids']):
+                list_id = lists[0].id
+                ids=[]
+                for line in obj.subscription_list_ids:
+                    ids.append(line.list_id)
+                if list_id not in ids:
+                    vals={
+                        'list_id'   : list_id,
+                        'contact_id': obj.id,
+                    }
+                    res = self.env['mailing.contact.subscription'].create(vals)
+                    print(obj, list_id,res)
+
