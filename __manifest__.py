7f1565b357b95ff2fb756adc0145f7769cdf70e7
Tony Galmiche <tony.galmiche@infosaone.com>
2025-07-10 08:03:35 +0200

    modifs

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Module odoo 13 pour France Filets",
-    "summary": "Module odoo 13 pour France Filets",
+    "summary": "Module odoo 13 pour France Filets ",
     "version": "13.0.1.0.0",
     "author": "Tony Galmiche / InfoSaône",
     "website": "https://infosaone.com",
